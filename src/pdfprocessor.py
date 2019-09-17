a581845b20e83a9a07b738a8fb9935bfcf19f48a
Rahul Jain <talentediq@gmail.com>
2019-09-17 08:50:22 +0530

    Submitting Processed file and script

--- src/pdfprocessor.py
+++ src/pdfprocessor.py
@@ -6,6 +6,8 @@
 from pdf2image import convert_from_path
 import os
 import sys
+import pytesseract
+import io
 
 def convertToImage(pdfFile, outputDir):
     pages = convert_from_path(pdfFile) #('../Data/test.pdf')
@@ -21,6 +23,18 @@ def convertToImage(pdfFile, outputDir):
         page.save(imgFile, 'JPEG')
         count += 1
 
+def processOCR(img):
+    print img
+    a = pytesseract.image_to_string(img, lang='hin+eng')
+    f = io.open(img + ".txt", "w+", encoding='utf8')
+    f.write(a)
+    f.close()
+
 if __name__ == "__main__":
     print (sys.argv)
-    convertToImage(sys.argv[1], "./outputs/")
+    allFiles = os.listdir("outputs/")
+    jpg_files = filter(lambda x: x[-4:] == '.jpg', allFiles)
+    for i in jpg_files:
+        #processOCR("./outputs/book_page200.jpg")
+        processOCR("./outputs/" + i)
+    #convertToImage(sys.argv[1], "./outputs/")
